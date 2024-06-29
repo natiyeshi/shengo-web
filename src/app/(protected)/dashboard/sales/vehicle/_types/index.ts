1aa198f4127959fb47593998c604fd78feaf8d5e
Natnael Yeshiwas <natiyeshi16@gmail.com>
2024-06-29 21:53:49 +0300

    add residence

--- src/app/(protected)/dashboard/sales/vehicle/_types/index.ts
+++ src/app/(protected)/dashboard/sales/vehicle/_types/index.ts
@@ -1,3 +1,3 @@
-import { SALES_TABS_MAP } from "../../_utils/constants";
+import { SALES_RESIDENCE_TABS_MAP } from "../../_utils/constants";
 
-export type SaleVehicleTabKeys = keyof typeof SALES_TABS_MAP;
+export type SaleResidenceTabKeys = keyof typeof SALES_RESIDENCE_TABS_MAP;
