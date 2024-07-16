49bf4334edaf651b6e3df2421b6f88b385720a56
Natnael Yeshiwas <natiyeshi16@gmail.com>
2024-07-16 22:00:20 +0300

    add pdf

--- src/components/custom/button-with-icon.tsx
+++ src/components/custom/button-with-icon.tsx
@@ -15,12 +15,14 @@ type Props = {
     | null
     | undefined;
   size?: "default" | "sm" | "lg" | "icon" | null | undefined;
+  onClick? : Function;
 };
 
-const ButtonWithIcon = ({ children, className, variant, size }: Props) => {
+const ButtonWithIcon = ({ children, className, variant, size,onClick }: Props) => {
   return (
     <Button
       variant={variant}
+      onClick={() => onClick && onClick()}
       size={size}
       className={cn("flex items-center gap-3", className)}
     >
